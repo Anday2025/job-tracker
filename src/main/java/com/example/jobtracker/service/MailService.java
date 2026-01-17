da64a7024e341a1d0940c1baca9a0fc3f9ef6fbd
h669832 <669832@stud.hvl.no>
2026-01-17 17:23:37 +0100

    Oppdatert mailService

--- src/main/java/com/example/jobtracker/service/MailService.java
+++ src/main/java/com/example/jobtracker/service/MailService.java
@@ -6,17 +6,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
+
     private final JavaMailSender mailSender;
 
     public MailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-    public void sendVerificationEmail(String to, String verifyUrl) {
+    public void sendVerificationEmail(String to, String link) {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(to);
         msg.setSubject("Bekreft e-post for Jobbsøker-tracker");
-        msg.setText("Klikk lenken for å aktivere brukeren din:\n\n" + verifyUrl);
+        msg.setText("Hei!\n\nKlikk her for å aktivere brukeren din:\n" + link + "\n\nHilsen Jobbsøker-tracker");
+
+        // valgfritt: setFrom, hvis du har verifisert domene (ellers kan sandbox håndtere)
+        // msg.setFrom("Jobbsøker-tracker <postmaster@sandboxXXX.mailgun.org>");
+
         mailSender.send(msg);
     }
 }

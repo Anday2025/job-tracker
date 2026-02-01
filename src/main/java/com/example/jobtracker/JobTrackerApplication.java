636df91c165250811a4d6b8f521141cda7d23be0
h669832 <669832@stud.hvl.no>
2026-02-01 21:14:40 +0100

    slette brukte,utløpte verifiseringstokens,

--- src/main/java/com/example/jobtracker/JobTrackerApplication.java
+++ src/main/java/com/example/jobtracker/JobTrackerApplication.java
@@ -2,12 +2,13 @@ package com.example.jobtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class JobTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobTrackerApplication.class, args);
 	}
-
 }
